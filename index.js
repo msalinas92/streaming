a2d033acdfaf5bd255ca3752b3b3535d4953d75c
Matias Salinas <msalinas61192@gmail.com>
2018-01-12 20:30:17 -0300

    Actualizacion

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
- 
+
 var express = require('express'),
-cluster = require('express-cluster'),
-fs = require('fs'),
-app = express(),
-compression = require('compression');
+    cluster = require('express-cluster'),
+    fs = require('fs'),
+    app = express(),
+    compression = require('compression'),
+    serveIndex = require('serve-index'),
+    fs = require("fs"),
+    path = require("path");
 
 console.log('Servidor iniciado');
 cluster((worker) => {
+    app.engine('jade', require('jade').__express)
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+    app.use('/assets', express.static('assets'))
     app.use(compression());
-    app.get('/:name', (req, res) => {
-        const path = 'videos/'+req.params.name+'.mp4';
+    app.get('/', (req, res) => {
+        var dir = __dirname + '/videos';
+        var list = fs.readdirSync(dir);
+        res.render('index', { 'list': list });
+    });
+    app.get('/videos/:name', (req, res) => {
+        const path = 'videos/' + req.params.name;
         const stat = fs.statSync(path);
         const fileSize = stat.size;
         const range = req.headers.range;
         if (range) {
             const parts = range.replace(/bytes=/, "").split("-")
             const start = parseInt(parts[0], 10)
-            const end = parts[1] 
+            const end = parts[1]
                 ? parseInt(parts[1], 10)
-                : fileSize-1
-            const chunksize = (end-start)+1
-            const file = fs.createReadStream(path, {start, end})
+                : fileSize - 1
+            const chunksize = (end - start) + 1
+            const file = fs.createReadStream(path, { start, end })
             const head = {
                 'Content-Range': `bytes ${start}-${end}/${fileSize}`,
                 'Accept-Ranges': 'bytes',
                 'Content-Length': chunksize,
-                'Content-Type':  'text/event-stream',
+                'Content-Type': 'text/event-stream',
             }
             res.writeHead(206, head);
             file.pipe(res);
@@ -40,4 +52,4 @@ cluster((worker) => {
     });
 
     return app.listen(3000);
-}, {count: 3});
+}, { count: 3 });
